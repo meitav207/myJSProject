cba026422cf85244b0bab8f2736e40d16f9682e0
meitav207 <meitav1010@gmail.com>
2025-04-15 14:38:38 +0300

    update js file with the les 1 tasks

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-const http = require('http');
+"use strict";
+//document.writeln("Meitav");
+//document.writeln("Itah"); //task 1
+
+//console.log('Meitav Itah');
+
+//const myArr = ["Meitav", "Ben", "Igor"];
+//console.log(myArr); // task 2
+
+//let name = prompt ('what is your name?')
+//alert('you name is ')
+/*const name = prompt("Enter your name:");
+alert(name);*/
+
+/*const flag = confirm("Are you sure you want to delete this item?");
+if(flag){
+    alert("item was deleted!");
+}
+else
+  alert("keep shoping");*/ //task3
+
+  //let name ="John" ,  admin=name;
+  //console.log(admin); //task 4
+  
+  
 
 
-const server = http.createServer(function (req, res) {
 
-});
-
-server.listen(3000);
