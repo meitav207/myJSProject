7ad647a4dcadddeeac3ce15b2e644820ea46c655
meitav207 <meitav1010@gmail.com>
2025-04-23 20:55:43 +0300

    update app.js for tasks (string methods)

--- app.js
+++ app.js
@@ -21,8 +21,157 @@ else
 
   //let name ="John" ,  admin=name;
   //console.log(admin); //task 4
+
+    //task1
+/*let name = 'Mary'
+let activity = 'drink tea'
+let bio = 'Out guest' + name + activity + '.'
+console.log(bio.length);*/
+
+
+//task2
+
+/*const str= 'It is a long establishing fact that a reader will be distract'
+const num = prompt('Enter the number of char in string');
+if(isNaN(num)){
+  console.log("Not number")
+}else{
+  console.log(`char #${num} is: "${str.charAt(num)}" `)
+}*/
+
+   //task3
+/*const str = prompt('enter any string and i will show the last chat and previous last chat')
+console.log(str[str.length-1] + str[str.length-2])*/
+
+//task4
+/*const str='abcde'
+let newstr=''
+for(let i=str.length-1;i>=0;i--)
+  {
+   newstr+= str[i];
+}
+  console.log(newstr);*/
+  
+  //task4
+  /*const str = "Hello , Word!"
+  console.log(str.toLowerCase());*/
+  
+ /* const str = "Hello , Word!"
+  console.log(str.toUpperCase());*/
+
+  //task5
+  /*const str='js'
+  console.log(str.toUpperCase());*/
+
+  /*const str = "   Hello, World!   "
+  const trimstr= str.trim();
+  console.log(trimstr);
+
+  const str1= "    Hello    ";
+  const str2= " \t\tWrorld!\t\t";
+  const str3= "\n\nJavaScript\n\n"
+  
+  console.log(str1.trim());
+  console.log(str2.trim());
+  console.log(str3.trim());
+
+
+  //task6
+  const res = 'abcde'.includes('ab')
+  console.log(res)
+
+  const res1 = 'ab cd ef'.includes('cd' , 5)
+  console.log(res1)*/
+
+  /*const str='hello world my name is meitav'
+  const userstr= prompt("type an string to check if includes in my string")
+  console.log(str.includes(userstr))*/
+
+  /*const str = 'abcde';
+  const newstr = str.replace('ab', '!')
+
+  console.log(newstr);
+
+  const str1 = 'ab cde ab'
+  let newstr1 = str1.replace('ab', '!s)
+  console.log(newstr1);*/
+
+
+  //task7
+  /*const elem ='-';
+  let str='1-2-3-4-5'
+  while(str.includes(elem)){
+    str = str.replace(elem,'.');
+  }
+  console.log(str);*/
+
+
+  //task8
+  /*const str='1-2-3-4-5'
+  const newstr = str.replaceAll('-','.')
+  console.log(newstr);*/
+
+  //const str='abcde'
+  //console.log(str.substr(0,3));
+  //const str='abcde'
+  //console.log(str.substr(2));
+  
+  //task9
+//const str = 'sdfwe'
+//console.log(str.substring(4,2)); //fw
+
+
+//task10
+/*const str= "i'm learning javascript!";
+console.log(str.substr(4,8)+str.substr(12));
+console.log(str.substring(4,12) + str.substring(12))
+console.log(str.slice(4,12)+str.slice(12))*/
+
+
+//task11
+//const str = 'http://www.notion.so/String-820cf1e906884d8fb9c02236fe1c6dcb/'
+//console.log(str.indexOf('http://'));
+
+
+//task12
+//const str = 'https://www.notion.html'
+//console.log(str.indexOf('.html',18));
+
+
+//task13
+//const str= 'ab cd cd cd ef';
+//console.log(str.lastIndexOf('cd')); //9
+//console.log(str.lastIndexOf('cd',8)); //6
+  
+
+//task14
+  //const str = 'abcde';
+
+  //console.log(str.startsWith('abc')); //true
+  //console.log(str.startsWith('xxx')); //false
+  //console.log(str.startsWith('bc',1)); //true
+  
+
+  //task15
+//const str = 'abcde'
+
+//console.log(str.endsWith('cde'));//true
+//console.log(str.endsWith('xxx'));//false
+//console.log(str.endsWith('bc',3));//true
+
+//task16
+//let str = 'ab-cd-ef'
+//console.log(str.split('-'))//'ab','cd','ef'
+//console.log(str.split('-',2)) // 'ab','cd'
+//console.log(str.split(''))//'a','b','-','c','d''-','e','f'
+//console.log(str.split('',3))//'a','b','-'
+
   
   
 
+
+
+    
+  
 
 
